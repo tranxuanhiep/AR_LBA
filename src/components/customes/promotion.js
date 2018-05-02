4ff4db688075fe29260865daa6f0b0f188d3da4e
Hiep (HilBert) X. TRAN <tranxuanhiep21095@gmail.com>
2018-05-02 09:07:19 +0700

    fix time ago

--- src/components/customes/promotion.js
+++ src/components/customes/promotion.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PhotoGrid from "react-native-thumbnail-grid";
 const { height, width } = Dimensions.get("screen");
-import moment from "moment";
+import TimeAgo from "react-native-timeago";
 const FBSDK = require("react-native-fbsdk");
 import { Image, Dimensions, Alert } from "react-native";
 import {
@@ -50,31 +50,40 @@ export const promotion = (item, props) => {
       contentDescription: "Wow, check out this great site!"
     }
   };
-  return <Card style={{ flex: 0 }}>
+  return (
+    <Card style={{ flex: 0 }}>
       <CardItem>
         <Left>
           <Thumbnail source={{ uri: item.Store_ImageLink }} />
           <Body>
             <Text>{item.Store_Name}</Text>
-            <Text note>{moment(item.Promotion_Created).fromNow()}</Text>
+            <TimeAgo time={item.Promotion_Created} />
           </Body>
         </Left>
       </CardItem>
       <CardItem>
         <Body>
-          <PhotoGrid height={200} width={width * 0.9} source={listImage} onPressImage={() => {}} />
+          <PhotoGrid
+            height={200}
+            width={width * 0.9}
+            source={listImage}
+            onPressImage={() => {}}
+          />
           <Text style={{ fontSize: 15, fontWeight: "bold" }}>
             {item.Promotion_Title}
           </Text>
           <Text style={{ fontSize: 15, fontWeight: "bold" }}>
-            {new Date(item.Promotion_DateStart).toDateString()} -- {new Date(item.Promotion_DateEnd).toDateString()}
+            {new Date(item.Promotion_DateStart).toDateString()} --{" "}
+            {new Date(item.Promotion_DateEnd).toDateString()}
           </Text>
           <Text style={{ fontSize: 15 }}>{item.Promotion_Description}</Text>
         </Body>
       </CardItem>
       <CardItem>
         <Left>
-          <Button transparent onPress={() => {
+          <Button
+            transparent
+            onPress={() => {
               if (props.proFile.id == null) {
                 Alert.alert(
                   "Rating",
@@ -93,28 +102,49 @@ export const promotion = (item, props) => {
                   ],
                   { cancelable: false }
                 );
-              } else props.favorite(item.Promotion_ID, props.proFile.id, item.Store_Details_ID);
-            }}>
-            <Image style={{ height: 20, width: 20 }} source={item.checkFavorite ? require("../../images/TabBar/FavoriteChoose.png") : require("../../images/TabBar/FavoriteNull.png")} />
-            <Text style={{ fontSize: 11 }}>
-              {item.TotalFavorite} Favorite
-            </Text>
+              } else
+                props.favorite(
+                  item.Promotion_ID,
+                  props.proFile.id,
+                  item.Store_Details_ID
+                );
+            }}
+          >
+            <Image
+              style={{ height: 20, width: 20 }}
+              source={
+                item.checkFavorite
+                  ? require("../../images/TabBar/FavoriteChoose.png")
+                  : require("../../images/TabBar/FavoriteNull.png")
+              }
+            />
+            <Text style={{ fontSize: 11 }}>{item.TotalFavorite} Favorite</Text>
           </Button>
         </Left>
         <Body>
           <Button transparent>
-            <Image style={{ height: 20, width: 20 }} source={require("../../images/comment.png")} />
+            <Image
+              style={{ height: 20, width: 20 }}
+              source={require("../../images/comment.png")}
+            />
             <Text style={{ fontSize: 11 }}>{item.TotalComment} Comments</Text>
           </Button>
         </Body>
         <Right>
-          <Button transparent onPress={() => {
+          <Button
+            transparent
+            onPress={() => {
               this.shareLinkWithShareDialog();
-            }}>
-            <Image style={{ height: 20, width: 20 }} source={require("../../images/share.png")} />
+            }}
+          >
+            <Image
+              style={{ height: 20, width: 20 }}
+              source={require("../../images/share.png")}
+            />
             <Text style={{ fontSize: 11 }}> Share</Text>
           </Button>
         </Right>
       </CardItem>
-    </Card>;
+    </Card>
+  );
 };
